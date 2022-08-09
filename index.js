87b4a54669012fb42fc81df1ae0ac601192aedf9
Diana Sushik <dianasushik2000@gmail.com>
2022-08-08 23:44:08 -0700

    commit tree a4335a5a6989a80a2e32f884704d6fb2c032f047

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = 'BOB';
+    return customerName;
+}
+
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+const leastFavoriteCustomer = 'jill';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'joe';
+    return leastFavoriteCustomer;
+}
